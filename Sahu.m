432996e056a322408d44a104b53d2f01f7ba1a7c
SameerSahu456 <41319235+SameerSahu456@users.noreply.github.com>
2018-07-17 12:56:06 +0530

    Add files via upload

---
+++ Sahu.m
@@ -0,0 +1,25 @@
+s = imread('sehwag.jpg')
+s1 = rgb2gray(s)
+
+p = imread('sehwag c.jpg')
+p1 = rgb2gray(p)
+
+entropy (p1)
+entropy (s1)
+
+figure(1);
+imshow(s)
+
+figure(2);
+imshow(s1)
+
+figure(3);
+imshow(p)
+
+figure(4);
+imshow(p1)
+
+
+b=randn;
+figure(5);
+imshow(b)
